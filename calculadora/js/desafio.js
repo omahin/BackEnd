478cdf2522ad11c045e3c1252ceb443baa5ec4dc
Olga Mahin <olgamahin@hotmail.com>
2021-05-26 02:37:02 -0300

    Reprograma workshop Backend

---
+++ calculadora/js/desafio.js
@@ -0,0 +1,15 @@
+var inputValorHora = document.querySelector('#valor-hora')
+var inputHorasProjeto = document.querySelector('#horas-projeto')
+
+var resultado = document.querySelector('span')
+
+function calcular() {
+    var valorHora = inputValorHora.valueAsNumber
+    var horasProjeto = inputHorasProjeto.valueAsNumber
+    var valorProjeto = valorHora * horasProjeto
+    var valorDuasCasas = valorProjeto.toFixed(2)
+
+    resultado.textContent = "R$ " + valorDuasCasas
+
+    console.log(resultado)
+}
---
+++ exemplo/script.js
@@ -0,0 +1,14 @@
+console.log('hello, Reprograma')
+
+var entrada = document.querySelector('input')
+
+var ano = entrada.value
+
+var saida = document.querySelector('#respostaAno')
+
+//saida.textContent = 0
+
+function cliquei (){
+   var ano = entrada.value
+   saida.textContent = ano
+}
